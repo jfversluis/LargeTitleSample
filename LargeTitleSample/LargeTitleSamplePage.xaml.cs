e1a0e3d263786ca62db154ee668b4dfef9f20bef
Gerald Versluis <gerald@verslu.is>
2017-09-21 22:11:11 +0200

    Initial commit

---
+++ LargeTitleSample/LargeTitleSamplePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LargeTitleSample
+{
+    public partial class LargeTitleSamplePage : ContentPage
+    {
+        public List<string> Items => new List<string>()
+        {
+			"a",
+			"b",
+			"c",
+			"d",
+			"e",
+			"f",
+			"g",
+			"h",
+			"i",
+			"j",
+			"k",
+			"l",
+			"m",
+			"n",
+			"o",
+			"p",
+			"q",
+			"r",
+			"s",
+			"t",
+			"u",
+			"v",
+			"w",
+			"x",
+			"y",
+			"z"
+        };
+
+        public LargeTitleSamplePage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ iOS/NavBarRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LargeTitleSample;
+using LargeTitleSample.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(NavBarRenderer))]
+namespace LargeTitleSample.iOS
+{
+    public class NavBarRenderer : NavigationRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            NavigationBar.PrefersLargeTitles = true;
+        }
+    }
+}
